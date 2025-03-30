32af7ad291aa4fa8f7178dd6a9a50fe70c3cc9a8
JasonShuyinta <jasonshuyinta@gmail.com>
2025-03-30 20:04:49 +0200

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/tendaluxe-italiano/',
+  // base: '/tendaluxe-italiano',
+  base: '/',
   server: {
     host: "0.0.0.0",
     port: 8080,
